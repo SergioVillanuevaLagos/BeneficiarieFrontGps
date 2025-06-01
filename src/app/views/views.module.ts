2827179bd356ef03d7cd1818505128f1abe8a110
SergioVillanuevaLagos <sergio.villanueva2101@alumnos.ubiobio.cl>
2025-06-01 14:27:36 -0400

    a
    a

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -7,11 +7,15 @@ import { ViewsRoutingModule } from './views-routing.module';
 import { FormsModule } from '@angular/forms';
 
 import { HomeComponent } from './home/home.component';
+import { SideBardComponent } from '../shared/components/side-bard/side-bard.component';
+import { UserListComponent } from '../shared/components/user-list/user-list.component';
 
 @NgModule({
   declarations: [
     BeneficiariesComponent,
     HomeComponent,
+    SideBardComponent,
+    UserListComponent
   ],
 
   imports: [
